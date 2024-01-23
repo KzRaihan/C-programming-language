c2a426b6f56d4d1a035fb0990528b72794138f2f
KzRaihan <kamruzzamanraihan00@gmail.com>
2024-01-24 00:09:52 +0600

    problems solution

--- CAPTER_O1/SUM_TWO_NUMBER..c
+++ CAPTER_O1/SUM_TWO_NUMBER..c
@@ -1,5 +1,10 @@
 /*
-    Take two integer from user then display the summation
+    Take two integer from user then display the Following operation.
+    -> Addition
+    -> Subtraction
+    -> Multiplication
+    -> Division
+
 */
 #include <stdio.h>
 
@@ -14,11 +19,19 @@ int main()
     scanf("%d", &num2);
 
     // Calculation
-    int sum = num1 + num2;
+    int sum = num1 + num2; // Addition
+    int sub = num1 - num2; // Subtraction
+    int mul = num1 * num2; // Multiplication
+    int div = num1 / num2; // Division
+
     // Display the output
     printf("\nThe First Number: %d", num1);
     printf("\nThe Second Number: %d", num2);
-    printf("\nSummation of the Numbers: %d", sum);
+
+    printf("\n Summation of the Numbers: %d", sum);
+    printf("\n Subtraction of the Numbers: %d", sub);
+    printf("\n Multiplication of the Numbers: %d", mul);
+    printf("\n Division of the Numbers: %d", div);
 
     return 0;
 }
